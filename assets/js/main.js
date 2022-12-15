8e32e6138954b8a010ea52032e705998a84fe0ab
cameronclass <kamrankutumov@gmail.com>
2022-12-15 09:57:12 +0300

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  const headerCatalogBtn = document.querySelector(".js-open-catalog");
 
+  if (headerCatalogBtn) {
+    headerCatalogBtn.addEventListener("click", () => {
+      const hamburger = document.querySelector(".js-hamburger");
+      hamburger.classList.toggle("is-active");
+    });
+  }
+});
