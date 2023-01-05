493de1744f1e80925bbf65a54dd55ae1fc7597aa
cameronclass <kamrankutumov@gmail.com>
2023-01-05 21:48:21 +0300

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,25 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+  /* Header Catalog */
   const headerCatalogBtn = document.querySelector(".js-open-catalog");
 
   if (headerCatalogBtn) {
     headerCatalogBtn.addEventListener("click", () => {
       const hamburger = document.querySelector(".js-hamburger");
       hamburger.classList.toggle("is-active");
+    });
+  }
+
+  /* Swiper */
+  if (document.querySelector(".main-products__tags_slider")) {
+    new Swiper(".main-products__tags_slider", {
+      slidesPerView: "auto",
+      spaceBetween: 24,
+      centeredSlides: true,
+      loop: true,
+      navigation: {
+        nextEl: ".main-products__tags .swiper-button-next",
+        prevEl: ".main-products__tags .swiper-button-prev",
+      },
     });
   }
 });
