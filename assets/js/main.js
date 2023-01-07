2feed68b73b419c5cff242b74da39c52d873b52b
cameronclass <kamrankutumov@gmail.com>
2023-01-07 20:55:41 +0300

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,4 +22,50 @@ window.addEventListener("DOMContentLoaded", (event) => {
       },
     });
   }
+
+  if (document.querySelector(".main-products-slider__slider .swiper")) {
+    new Swiper(".main-products-slider__slider .swiper", {
+      slidesPerView: "auto",
+      spaceBetween: 24,
+      centeredSlides: true,
+      loop: true,
+      navigation: {
+        nextEl: ".main-products-slider__slider .swiper-button-next",
+        prevEl: ".main-products-slider__slider .swiper-button-prev",
+      },
+    });
+  }
+
+  if (document.querySelector(".main-reviews__slider .swiper")) {
+    new Swiper(".main-reviews__slider .swiper", {
+      slidesPerView: 3,
+      spaceBetween: 35,
+      loop: true,
+      navigation: {
+        nextEl: ".main-reviews__slider .swiper-button-next",
+        prevEl: ".main-reviews__slider .swiper-button-prev",
+      },
+    });
+  }
+
+  if (document.querySelector(".main-card-slider")) {
+    new Swiper(".main-card-slider", {
+      slidesPerView: 1,
+      spaceBetween: 24,
+      pagination: {
+        el: ".main-card-slider .swiper-pagination",
+        clickable: true,
+      },
+    });
+
+    document
+      .querySelectorAll(".main-card-slider .swiper-pagination-bullet")
+      .forEach((el) =>
+        el.addEventListener("mouseover", (event) => {
+          el.click();
+        })
+      );
+  }
+
+  new Accordion(".accordion-container");
 });
